cc2c13d0cb99e8b38165c222883342e34559f27a
Gordon Kennedy <gkennedy@yandex.ru>
2018-02-28 17:34:50 +0200

    Version included

---
+++ GDK++/versioninfo.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------
+  VERSION CONTROL BUILD SYSTEM                               
+  This header file was created by VERBUILD v1.0.1            
+  -----------------------------------------------------------
+  help : verbuild -?                                         
+  info : http://www.yeamaec.com                              
+         yeamaec@hanafos.com ,krkim@yeamaec.com              
+-----------------------------------------------------------*/
+
+#ifndef VERSIONNO__H
+#define VERSIONNO__H
+
+#define VERSION_FULL           0.1.0.0
+
+#define VERSION_BASEYEAR       
+#define VERSION_DATE           
+#define VERSION_TIME           
+
+#define VERSION_MAJOR          0
+#define VERSION_MINOR          1
+#define VERSION_BUILDNO        0
+#define VERSION_EXTEND         0
+
+#define VERSION_FILE           1,0,0,0
+#define VERSION_PRODUCT        1,0,0,0
+#define VERSION_FILESTR        "1,0,0,0"
+#define VERSION_PRODUCTSTR     "1,0,0,0"
+
+#endif
