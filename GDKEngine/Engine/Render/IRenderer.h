0dcec333bb88d48f879b7772456bfe63ad0be7ae
Gordon Kennedy <gkennedy@yandex.ru>
2018-02-28 20:10:01 +0200

    Small fix for renderer interface and ability for switching between DX and OGL

--- GDKEngine/Engine/Render/IRenderer.h
+++ GDKEngine/Engine/Render/IRenderer.h
@@ -3,12 +3,17 @@
 
 class IRenderer 
 {
-	virtual void Init(HWND hwnd);
-	virtual void SwapBuffers();
-	virtual void DrawLine();
-	virtual void DrawMesh();
-	virtual void DrawString();
-	virtual void DrawSurface();
-	virtual void DrawImage();
-	virtual void Release();
+	enum RendererType
+	{
+		OpenGL,
+		DirectX
+	};
+	virtual void Init(HWND hwnd) = 0;
+	virtual void SwapBuffers() = 0;
+	virtual void DrawLine() = 0;
+	virtual void DrawMesh() = 0;
+	virtual void DrawString() = 0;
+	virtual void DrawSurface() = 0;
+	virtual void DrawImage() = 0;
+	virtual void Release() = 0;
 };
