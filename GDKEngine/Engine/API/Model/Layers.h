c0de06870825ac25b131f58f750fa11b5374d39c
Serega V <serega.vetukh@gmail.com>
2018-03-11 14:40:44 +0200

    +Layers

---
+++ GDKEngine/Engine/API/Model/Layers.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef GDK_LAYERS_H
+#define GDK_LAYERS_H
+
+const int	GDK_LAYER_DEFAULT	= 0,
+			GDK_LAYER_EDITOR	= -1,
+			GDK_LAYER_SKY		= 1,
+			GDK_LAYER_WATER		= 2,
+			GDK_LAYER_CUSTOM	= 3;
+
+//--------------------------- USAGE -------------------------------
+// enum MyLayers
+// {
+//	  MY_LAYER = GDK_LAYER_CUSTOM + 1;
+// }
+
+enum Layer 
+{
+	DEFAULT = GDK_LAYER_DEFAULT,
+	EDITOR = GDK_LAYER_EDITOR,
+	SKY	= GDK_LAYER_SKY,
+	WATER = GDK_LAYER_WATER,
+	CUSTOM = GDK_LAYER_CUSTOM
+};
+#endif
